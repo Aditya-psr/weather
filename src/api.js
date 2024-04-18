4b93605880f67508aae48abfbf21e810d95f6b01
Aditya-psr <adityapratapsinghravat30@gmail.com>
2024-04-19 02:17:52 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "236e9b7672msh0954c53d5f11acep14c4c2jsne0ca2d0dbdb1",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d5735a9ceb8d814d3ee85cc9c3d5d088";
